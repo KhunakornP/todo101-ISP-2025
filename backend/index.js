6d3e66e9d4f7c35b8b2ef7122cb5cd46a9e4f9e1
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-08-10 19:53:41 +0700

    Implement backend

---
+++ backend/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = 15000;
+
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/todos'
+
+app.use(cors());
+app.use(express.json());
+
+const Task = mongoose.model('Task', new mongoose.Schema({
+    text: String,
+    completed: Boolean
+}));
+
+app.get('/tasks', async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+})
+
+app.post('/tasks', async (req, res) => {
+    const tasks = await Task.create(req.body);
+    res.json(tasks);
+})
+
+app.put('/tasks/:id', async (req, res) => {
+    const tasks = await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.json(tasks);
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    const tasks = await Task.findByIdAndDelete(req.params.id);
+    res.sendStatus(204)
+})
+
+const connectToDB = () => {
+    console.log("Trying to connect to MongoDB...")
+    mongoose.connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+      .then(() => {
+        console.log('Mongo connected')
+        app.listen(port, () => {
+            console.log(`app running on port ${port}`)
+        })
+      })
+      .catch( err => {
+        console.log('DB go BOOOM', err.message);
+        setTimeout(connectWithRetry, 500);
+      })
+}
+
+connectToDB()
